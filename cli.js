6ecdc0ad53232a418f982aa8ed215499f14eccd6
Nephtali Rodriguez <nukz45@gmail.com>
2018-10-05 16:15:50 -0400

    Feature/add to release (#12)
    * Adds initial working functionality of adding to release
    
    * Adds final touches to tests
    
    * Adds some final fixes to command description
    
    * Adds version to help

--- cli.js
+++ cli.js
@@ -4,9 +4,17 @@
 const prog = require('caporal');
 const Hoek = require('hoek');
 const path = require( 'path' );
+const colorLogger = require('node-color-log');
+const version = require('./package.json').version;
 
-const githubClient = require('./lib/modules/github_client');
-const changelogGenerator = require('./lib/modules/changelog_generator');
+const {
+    githubClient,
+    changelogGenerator,
+    addToRelease
+} = require('./lib/modules');
+// const githubClient = require('./lib/modules/github-client');
+// const changelogGenerator = require('./lib/modules/changelog-generator');
+// const addToRelease = require('./lib/modules/add-to-release');
 const helpers = require('./lib/modules/helpers');
 
 const requireConfig = () => {
@@ -20,7 +28,8 @@ const requireConfig = () => {
 };
 
 prog
-    .version('1.0.0')
+    .version(version)
+    .logger(colorLogger)
     .command('generate', 'Generates the changelog')
     .option('--github_api <apiUrl>', 'Github API URL (Used with Github Enterprise)')
     .option('--token <githubToken>', 'Github OAUTH Token')
@@ -31,22 +40,37 @@ prog
 
         return tags.split(',');
     })
+    .option('--release <pre>', 'Adds the given generated changelog to the release. If "pre" is passed, will mark as a pre-release')
     .action(async(args, options, logger) => {
         try {
             const config = requireConfig();
             const token = Hoek.reach(options, 'token') || Hoek.reach(config, 'github.token');
             const githubApi = Hoek.reach(options, 'githubApi') || Hoek.reach(config, 'github.apiUrl') || 'https://api.github.com';
             const tags = helpers.setTags(options.tags);
-            const context = {
-                config
-            };
+            let context;
+            let client;
+            let changelog;
 
             if (config || token && githubApi) {
-                const client = githubClient(githubApi, token);
-                await changelogGenerator.call(context, client, tags);
-                logger.info('Changelog Generated as CHANGELOG.md');
+                client = githubClient(githubApi, token);
+
+                context = {
+                    config,
+                    client,
+                    logger,
+                    tags
+                };
+
+                changelog = await changelogGenerator.call(context);
+                logger.color('green').bold().log('Changelog Generated as CHANGELOG.md');
             } else {
                 throw new TypeError('Error: Config options are not set in command line or in .origami-logs-config.json. See --help to learn what you can send.');
+            }
+
+            if (options.release) {
+                const release = await addToRelease.call(context, changelog, options.release);
+
+                logger.color('green').bold().log(`RELEASE GENERATED FOR ${release.data.html_url}`);
             }
         } catch (e) {
             logger.error(e.message);
--- test/integration.js
+++ test/integration.js
@@ -3,89 +3,139 @@
 const Lab = require('lab');
 const fs = require('fs');
 const sinon = require('sinon');
+const logger = require('node-color-log');
 const { script, assertions } = Lab;
 const lab = exports.lab = script();
 const { describe, it, expect, before, after } = lab;
-let githubClient = require('../lib/modules/github_client');
-const changelogGenerator = require('../lib/modules/changelog_generator');
+
+
+const {
+    changelogGenerator,
+    addToRelease
+} = require('../lib/modules');
+
 const config = require('../.origami-logs-config-template');
 
 assertions.should();
 
 describe('Integration', () =>{
-    const context = {
-        config
-    };
+    let context;
+    let githubClient;
 
     before(() =>{
         const writeToFile = sinon.fake.returns(true);
         sinon.replace(fs, 'writeFileSync', writeToFile);
+
+        const sampleCommit = {
+            sha   : '4be33ee7ba38ee00ec2b3f3d96e0bbba553fbd65',
+            commit: {
+                author: {
+                    name : 'JustNeph',
+                    email: 'justneph@xogrp.com',
+                    date : '2017-12-05T17:02:34Z'
+                },
+                committer: {
+                    name : 'Github',
+                    email: 'github@github.com',
+                    date : '2017-12-05T17:02:34Z'
+                },
+                message: '[#1234567] Adds a fix to properly return the internal mediaSummary … (#31)\n\n* [#153019762] Adds a fix to properly return the internal mediaSummary structure for Limited vendors\r\n\r\n* Changed let to const in test'
+            }
+        };
+
+        githubClient = {
+            repos: {
+                compareCommits: function() {
+                    return {
+                        data: {
+                            commits: [sampleCommit]
+                        }
+                    };
+                },
+                getReleaseByTag: function() {
+                    return {
+                        data: {
+                            id      : '12345',
+                            html_url: 'https://www.github.com/someOrg/justneph/pulls/12345'
+                        }
+                    };
+                },
+                editRelease: function() {
+                    return true;
+                }
+            },
+            pullRequests: {
+                get: function() {
+                    return Promise.resolve({
+                        data: {
+                            url : 'https://www.github.com/someOrg/justneph/pull/31',
+                            user: {
+                                login: 'nrodriguez',
+                                url  : 'https://api.github.com/users/nrodriguez'
+                            }
+                        }
+                    });
+                }
+            },
+            search: {
+                issues: function() {
+                    return {
+                        data: {
+                            total_count: 1,
+                            items      : [{
+                                name  : 'TEST',
+                                number: '1234',
+                                labels: [{
+                                    name: 'enhancement'
+                                }]
+                            }]
+                        }
+                    };
+                }
+            }
+        };
+
+        context = {
+            config,
+            client: githubClient,
+            tags  : {
+                tag1: 'v1.0.0',
+                tag2: 'v2.0.0'
+            },
+            logger
+        };
     });
 
     after(() =>{
         sinon.restore();
     });
 
-    const sampleCommit = {
-        sha   : '4be33ee7ba38ee00ec2b3f3d96e0bbba553fbd65',
-        commit: {
-            author: {
-                name : 'JustNeph',
-                email: 'justneph@xogrp.com',
-                date : '2017-12-05T17:02:34Z'
-            },
-            committer: {
-                name : 'Github',
-                email: 'github@github.com',
-                date : '2017-12-05T17:02:34Z'
-            },
-            message: '[#1234567] Adds a fix to properly return the internal mediaSummary … (#31)\n\n* [#153019762] Adds a fix to properly return the internal mediaSummary structure for Limited vendors\r\n\r\n* Changed let to const in test'
-        }};
-    githubClient = {
-        repos: {
-            compareCommits: function() {
-                return {
-                    data: {
-                        commits: [sampleCommit]
-                    }
-                };
-            }
-        },
-        pullRequests: {
-            get: function() {
-                return Promise.resolve({
-                    data: {
-                        url : 'https://www.github.com/someOrg/justneph/pull/31',
-                        user: {
-                            login: 'nrodriguez',
-                            url  : 'https://api.github.com/users/nrodriguez'
-                        }
-                    }
-                });
-            }
-        },
-        search: {
-            issues: function() {
-                return {
-                    data: {
-                        total_count: 1,
-                        items      : [{
-                            name  : 'TEST',
-                            number: '1234',
-                            labels: [{
-                                name: 'enhancement'
-                            }]
-                        }]
-                    }
-                };
-            }
-        }
-    };
 
     it('generates the CHANGELOG.md file with the expected changelog', async() =>{
         const expected = '**Features Implemented:**\n* [[#1234567]](https://www.pivotaltracker.com/n/projects/1234567/stories/1234567) Adds a fix to properly return the internal mediaSummary … [(#31)](https://www.github.com/someOrg/justneph/pull/31) [nrodriguez](https://api.github.com/users/nrodriguez)\n';
-        await changelogGenerator.call(context, githubClient, ['v1.2.1', 'HEAD']);
-        expect(fs.writeFileSync.called).to.be.true;
+        await changelogGenerator.call(context, ['v1.2.1', 'HEAD']);
         expect(fs.writeFileSync.lastArg).to.equal(expected);
+    });
+
+    it('pushes the releases notes to github', async() =>{
+        sinon.spy(githubClient.repos, 'getReleaseByTag');
+        sinon.spy(githubClient.repos, 'editRelease');
+
+        const changelog = '**Features Implemented:**\n* [[#1234567]](https://www.pivotaltracker.com/n/projects/1234567/stories/1234567) Adds a fix to properly return the internal mediaSummary … [(#31)](https://www.github.com/someOrg/justneph/pull/31) [nrodriguez](https://api.github.com/users/nrodriguez)\n';
+        await addToRelease.call(context, changelog, 'test');
+
+        expect(githubClient.repos.getReleaseByTag.getCall(0).lastArg).to.deep.equal({
+            owner: 'Organization',
+            repo : 'repo-name',
+            tag  : 'v2.0.0'
+        });
+
+        expect(githubClient.repos.editRelease.getCall(0).lastArg).to.deep.equal({
+            owner     : 'Organization',
+            repo      : 'repo-name',
+            release_id: '12345',
+            body      : changelog,
+            prerelease: false
+        });
     });
 });
