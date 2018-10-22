fcac213aa277390ef1bab451f24d1264e9415ebd
Nephtali Rodriguez <nukz45@gmail.com>
2018-10-22 13:28:18 -0400

    Feature/add in repo command (#15)
    * Adding in the ability to pass in repo as an cli arg
    
    * Trying merge
    
    * Adds fixes to config to properly overwrite if there is a config file

--- cli.js
+++ cli.js
@@ -24,12 +24,38 @@ const requireConfig = () => {
     }
 };
 
+const setupConfig = (options) => {
+    const configFile = requireConfig();
+    const token = Hoek.reach(options, 'token') || Hoek.reach(configFile, 'github.token');
+    const apiUrl = Hoek.reach(options, 'githubApi') || Hoek.reach(configFile, 'github.apiUrl') || 'https://api.github.com';
+    const repository = Hoek.reach(options, 'repo') || Hoek.reach(configFile, 'github.repository');
+
+    if (!configFile) {
+        return {
+            github: {
+                apiUrl,
+                token,
+                repository
+            }
+        };
+    }
+
+    return Hoek.merge(configFile, {
+        github: {
+            apiUrl,
+            token,
+            repository
+        }
+    });
+};
+
 prog
     .version(version)
     .logger(colorLogger)
     .command('generate', 'Generates the changelog')
     .option('--github_api <apiUrl>', 'Github API URL (Used with Github Enterprise)')
     .option('--token <githubToken>', 'Github OAUTH Token')
+    .option('--repo <repository>', 'Github Org/Repo "org/repo"')
     .option('--tags <tag1>,<tag2>', 'Gets the changelog between the tags', function(tags) {
         if (tags === true) {
             return [];
@@ -40,16 +66,14 @@ prog
     .option('--release <pre>', 'Adds the given generated changelog to the release. If "pre" is passed, will mark as a pre-release')
     .action(async(args, options, logger) => {
         try {
-            const config = requireConfig();
-            const token = Hoek.reach(options, 'token') || Hoek.reach(config, 'github.token');
-            const githubApi = Hoek.reach(options, 'githubApi') || Hoek.reach(config, 'github.apiUrl') || 'https://api.github.com';
+            const config = setupConfig(options);
             const tags = helpers.setTags(options.tags);
             let context;
             let client;
             let changelog;
 
-            if (config || token && githubApi) {
-                client = githubClient(githubApi, token);
+            if (config.github.token && config.github.apiUrl && config.github.repository) {
+                client = githubClient(config.github.apiUrl, config.github.token);
 
                 context = {
                     config,
